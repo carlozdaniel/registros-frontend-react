9550ed2e6cbb7ebf0e26a182be1619a793df56ee
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2020-12-30 02:01:27 -0600

    connection completed
    The connection of react with the application of rails was made by means
    of the axios library.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,37 @@
 import React from "react";
+import axios from "axios";
 
-function Home() {
-  return(
-    <div className="App">
-      <h1>hola</h1>
-    </div>
-  );
+class Ejemplo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      empresas: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:1500/api/v1/empresas").then(response => {
+      this.setState({
+        empresas: response.data
+      });
+    });
+  }
+
+  listEmpresas(){
+    return this.state.empresas.map(empresa => {
+      return(
+        <div key={empresa.id}>
+          <h1>{empresa.name}</h1>
+          {empresa.description}
+        </div>
+      );
+    });
+  }
+
+  render(){
+    return(
+      <div className="App">{this.listEmpresas()}</div>
+    );
+  }
 }
-export default Home;
+export default Ejemplo;
